8a4ba919fff4f8b1014887fc8f1d7551c8d5dea8
JuliaTitto87 <133301250+JuliaTitto87@users.noreply.github.com>
2024-01-16 17:41:30 +0300

    Model Employee created

---
+++ ListOfEmployees/Models/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ListOfEmployees.Models
+{
+    public class EmployeeViewModel
+    {
+        public int PersonnelNumber { get; set; }
+
+        public string FullName{ get; set; }
+
+        public string Department { get; set; }
+
+        public string EmployeesPosition { get; set; }
+    }
+}
